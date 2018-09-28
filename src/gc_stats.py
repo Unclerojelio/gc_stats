843ef32dcbd7c3270095ac0c3b5cc58009560e93
Roger Banks <roger_banks@mac.com>
2018-09-27 20:43:56 -0500

    Refactored into functions and added namespace dict.

--- src/gc_stats.py
+++ src/gc_stats.py
@@ -1,10 +1,69 @@
+# python save.py
+
 import xml.etree.ElementTree as ET
-
-ns = 'http://www.topografix.com/GPX/1/0'
-
 tree = ET.parse('../data/Adelaide.gpx')
 root = tree.getroot()
-for wpt in root.iter('{http://www.topografix.com/GPX/1/0}wpt'):
-    name = wpt.find('{http://www.topografix.com/GPX/1/0}name').text
-    type = wpt.find('{http://www.topografix.com/GPX/1/0}type').text
-    print name, type
+
+ns = {'gpx': 'http://www.topografix.com/GPX/1/0',
+      'gs': 'http://www.groundspeak.com/cache/1/0'}
+
+def num_caches():
+    caches = []
+    for wpt in root.findall('gpx:wpt', ns):
+        cache = wpt.find('gpx:type', ns).text
+        caches.append(cache)
+    return len(caches)
+
+def print_types():
+
+    traditional_caches = []
+    multi_caches = []
+    event_caches = []
+    letterbox_caches = []
+    earth_caches = []
+    event_caches = []
+    virtual_caches = []
+    other_caches = []
+    
+    for wpt in root.findall('gpx:wpt', ns):
+        name = wpt.find('gpx:name', ns).text
+        type = wpt.find('gpx:type', ns).text
+        if type == 'Geocache|Traditional Cache':
+            traditional_caches.append(name)
+        elif type == 'Geocache|Letterbox Hybrid':
+            letterbox_caches.append(name)
+        elif type == 'Geocache|Multi-cache':
+            multi_caches.append(name)
+        elif type == 'Geocache|Earthcache':
+            earth_caches.append(name)
+        elif type == 'Geocache|Event Cache':
+            event_caches.append(name)
+        elif type == 'Geocache|Virtual Cache':
+            virtual_caches.append(name)
+        else:
+            other_caches.append(name)
+    
+    print 'Traditional Caches: ' + str(len(traditional_caches))
+    print 'Letterbox Caches: ' + str(len(letterbox_caches))
+    print 'Multi Caches: ' + str(len(multi_caches))
+    print 'Earth Caches: ' + str(len(earth_caches))
+    print 'Event Caches: ' + str(len(event_caches))
+    print 'Virtual Caches: ' + str(len(virtual_caches))
+    print 'Other Caches: ' + str(len(other_caches))
+
+def print_list():
+    size = ''
+    for wpt in root.findall('gpx:wpt', ns):
+        number = wpt.find('gpx:name', ns).text
+        type = wpt.find('gpx:type', ns).text
+        name = wpt.find('gpx:urlname', ns).text
+        cache = wpt.find('gs:cache', ns)
+        size = cache.find('gs:container', ns).text
+        
+
+        print number + '\t' + name + '\t' + type.split('|')[1] + '\t' + size
+        
+
+#print num_caches()
+#print_types()
+print_list()
